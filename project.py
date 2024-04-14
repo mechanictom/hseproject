5f235940d10169eefc45573252e8b16ca34ddfb1
alex <alex@alexvo.ru>
2024-04-14 22:58:35 +0700

    fix

--- project.py
+++ project.py
@@ -178,7 +178,7 @@ fig, ax1 = plt.subplots()
 color = 'tab:red'
 ax1.set_xlabel('year')
 ax1.set_ylabel('USD RUB rate', color=color)
-ax1.set_ylim(0,150)
+ax1.set_ylim(20,100)
 ax1.bar(all_data.index, all_data.USDRUB_year_avg, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 
@@ -186,7 +186,7 @@ ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:blue'
 ax2.set_ylabel('Brent price', color=color)  # we already handled the x-label with ax1
-ax2.set_ylim(20,80)
+ax2.set_ylim(0,150)
 ax2.plot(all_data.index, all_data.BRENT_year_avg, color=color)
 
 ax2.tick_params(axis='y', labelcolor=color)
