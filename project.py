980d527eb1f6a16c9bee16402c4b64ccd49bcc7e
alex <alex@alexvo.ru>
2024-04-14 22:57:40 +0700

    minor change

--- project.py
+++ project.py
@@ -186,7 +186,9 @@ ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:blue'
 ax2.set_ylabel('Brent price', color=color)  # we already handled the x-label with ax1
+ax2.set_ylim(20,80)
 ax2.plot(all_data.index, all_data.BRENT_year_avg, color=color)
+
 ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
